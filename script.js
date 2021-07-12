88338acc7880d2138697bbddd366a2f77e1cba20
n3tman <n3tman@users.noreply.github.com>
2021-07-12 20:54:47 +0300

    Check if file is json, styles for dropzone

--- script.js
+++ script.js
@@ -11,9 +11,25 @@ Dropzone.options.upload = {
         });
     },
     accept: function(file, done) {
-        // console.log(file);
+        var reader = new FileReader();
+        reader.addEventListener('load', function(event) {
+            var result = event.target.result;
+            var json = {};
+            try {
+                json = JSON.parse(result);
+                if (json.hasOwnProperty('image') && json.image.trim()) {
+                    document.querySelector('.dz-image > img').src = json.image;
+                }
+            } catch(e) {
+                done('Invalid playlist file');
+            }
+        });
+        reader.readAsText(file);
     },
     maxFilesize: 1.2, // MB
-    addRemoveLinks: true
+    addRemoveLinks: true,
+    thumbnailWidth: 200,
+    thumbnailHeight: 200,
+    dictDefaultMessage: 'Drop your <b>.bplist</b> here<br>(or click to choose a file)',
     // autoProcessQueue: false
 };
